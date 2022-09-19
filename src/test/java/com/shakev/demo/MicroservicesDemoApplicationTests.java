c885fb8e121ced4f359a35dc4010076da36b8f60
Georgi Shakev <shakev00@gmail.com>
2022-09-19 19:11:48 +0300

    Initial commit

---
+++ src/test/java/com/shakev/demo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
