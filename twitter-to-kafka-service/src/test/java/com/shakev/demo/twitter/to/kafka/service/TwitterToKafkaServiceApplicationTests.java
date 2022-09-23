c3727e3fe862f74a5b38d76b58d3362ce8e94efe
Georgi Shakev <shakev00@gmail.com>
2022-09-23 17:15:59 +0300

    Create Main Project and  TwitterToKafkaService

---
+++ twitter-to-kafka-service/src/test/java/com/shakev/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakev.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+   @Test
+    public void loadContext(){
+
+   }
+}
